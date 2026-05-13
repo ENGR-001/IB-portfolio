ddbcf12cb4dbf5724c7e91830f8da769547ff214
IBRAHIM MAHAMMAD SANI <khalilmuhammadmjy008@gmail.com>
2026-05-13 19:58:45 +0100

    Add portfolio project files
    Added HTML, CSS, JavaScript, and images for my responsive portfolio website.

---
+++ app.js
@@ -0,0 +1,16 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll(){
+  reveals.forEach(element => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+
+    if(elementTop < windowHeight - 100){
+      element.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
